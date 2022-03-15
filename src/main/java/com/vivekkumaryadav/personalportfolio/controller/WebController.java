363748a27fe74cb0b360817eb66e49955d450aab
Vyadavgit <>
2022-03-14 21:45:33 -0500

    changes in templates

---
+++ src/main/java/com/vivekkumaryadav/personalportfolio/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.vivekkumaryadav.personalportfolio.controller;
+
+import com.vivekkumaryadav.personalportfolio.model.PersonalInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class WebController {
+    @GetMapping(value = "/")
+    public String index(Model model){
+        PersonalInfo personalInfo = new PersonalInfo();
+        personalInfo.setFullName("Vivek Kumar Ydav");
+        personalInfo.setAddress("Arlington, Texas");
+        personalInfo.setAge(23);
+        personalInfo.setEmail("abc@gmail.com");
+        personalInfo.setCvURL("https://www.google.com");
+        personalInfo.setDescription("This is test description");
+
+        model.addAttribute("personalInfo", personalInfo);
+        return "index";
+    }
+}
---
+++ src/main/java/com/vivekkumaryadav/personalportfolio/model/PersonalInfo.java
@@ -0,0 +1,15 @@
+package com.vivekkumaryadav.personalportfolio.model;
+
+import lombok.Data;
+
+@Data
+public class PersonalInfo {
+
+    private String fullName;
+    private String address;
+    private String email;
+    private int age;
+    private String description;
+    private String cvURL;
+
+}
